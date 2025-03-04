0dee1fa29e282122cb0a046b2533e88677c22fca
Pedro Fernández <pedrofm@pedrofm.dev>
2025-03-04 17:11:45 +0100

    parsing expressions 1

--- ast/ast.go
+++ ast/ast.go
@@ -1,40 +1,69 @@
 package ast
 
 import (
+	"bytes"
 	"pedroScript/token"
 )
 
+// Node interface represents a node in the AST.
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
+// Statement interface represents a statement node in the AST.
 type Statement interface {
 	Node
 	statementNode()
 }
 
+// Expression interface represents an expression node in the AST.
 type Expression interface {
 	Node
 	expressionNode()
 }
 
-// All the statements contained in a Array
+// ExpressionStatement represents an expression as a statement (e.g., 2 * 3).
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
+
+// Program represents a collection of statements (the root node of an AST).
 type Program struct {
 	Statements []Statement
 }
 
-// Gets the root token literal of the program
+// TokenLiteral returns the literal value of the first token in the program.
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
-	} else {
-		return ""
 	}
+	return ""
 }
 
-// LET implementation
+// String returns the string representation of the program's statements.
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
+// LetStatement represents a variable declaration (e.g., let x = 5;).
 type LetStatement struct {
-	Token token.Token 
+	Token token.Token
 	Name  *Identifier
 	Value Expression
 }
@@ -42,19 +71,44 @@ type LetStatement struct {
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+// Identifier represents an identifier in the AST (e.g., a variable name).
 type Identifier struct {
-	Token token.Token 
+	Token token.Token
 	Value string
 }
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string { return i.Value }
 
-// Return implementation 
+// ReturnStatement represents a return statement (e.g., return x;).
 type ReturnStatement struct {
-    Token token.Token
-    ReturnValue Expression
+	Token       token.Token
+	ReturnValue Expression
 }
 
-func (rs *ReturnStatement) statementNode() {}
+func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
--- parser/parser.go
+++ parser/parser.go
@@ -7,6 +7,17 @@ import (
 	"pedroScript/token"
 )
 
+const (
+	_ int = iota
+	LOWEST
+	EQUALS      // ==
+	LESSGREATER // > or <
+	SUM         // +
+	PRODUCT     // *
+	PREFIX      // -X or !X
+	CALL        // myFunction(X)
+)
+
 type Parser struct {
 	l *lexer.Lexer
 
@@ -14,10 +25,22 @@ type Parser struct {
 
 	curToken  token.Token
 	peekToken token.Token
+
+	prefixParseFns map[token.TokenType]prefixParseFn
+	infixParseFns  map[token.TokenType]infixParseFn
 }
+
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn  func(ast.Expression) ast.Expression
+)
 
 func New(l *lexer.Lexer) *Parser {
 	p := &Parser{l: l, errors: []string{}}
+
+	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
+	p.registerPrefix(token.IDENT, p.parseIdentifier)
+
 	// Read two tokens, so curToken and peekToken are both set
 	p.nextToken()
 	p.nextToken()
@@ -36,7 +59,6 @@ func (p *Parser) peekError(t token.TokenType) {
 }
 
 func (p *Parser) nextToken() {
-
 	p.curToken = p.peekToken
 	p.peekToken = p.l.NextToken()
 }
@@ -64,7 +86,7 @@ func (p *Parser) parseStatement() ast.Statement {
 	case token.RETURN:
 		return p.parseReturnStatement()
 	default:
-		return nil
+		return p.parseExpressionStatement()
 	}
 }
 
@@ -113,4 +135,40 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.peekError(t)
 		return false
 	}
+}
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
+}
+
+func (p *Parser) parseExpression(precedence int) ast.Expression {
+	prefix := p.prefixParseFns[p.curToken.Type]
+
+	if prefix == nil {
+		return nil
+	}
+
+	leftExp := prefix()
+
+	return leftExp
+}
+
+func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+	stmt := &ast.ExpressionStatement{Token: p.curToken}
+
+	stmt.Expression = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseIdentifier() ast.Expression {
+	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
 }
